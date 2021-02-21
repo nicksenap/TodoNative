4349b938ddebea33f6a90abe5e99df38d8b1a48a
nick <zs@lendify.se>
2021-02-21 23:31:10 +0100

    pt.2 done

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "./Colors";
+import tempData from "./tempData";
+import TodoList from "./components/TodoList";
+import AddListModel from "./components/AddListModal";
 
 export default function App() {
+  const [addTodoVisible, setAddTodoVisible] = useState(false);
+  const toggleAddTodoModal = () => setAddTodoVisible(!addTodoVisible);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal
+        animationType="slide"
+        visible={addTodoVisible}
+        onRequestClose={toggleAddTodoModal}
+      >
+        <AddListModel closeModal={toggleAddTodoModal} />
+      </Modal>
+
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Todo
+          <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity style={styles.addLists} onPress={toggleAddTodoModal}>
+          <AntDesign name="plus" size={16} color={colors.blue} />
+        </TouchableOpacity>
+        <Text style={styles.add}> Add Lists </Text>
+      </View>
+
+      <View style={{ height: 275, paddingLeft: 32 }}>
+        <FlatList
+          data={tempData}
+          keyExtractor={(item) => item.name}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <TodoList list={item} />}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +58,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "700",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addLists: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
---
+++ tempData.js
@@ -0,0 +1,48 @@
+const tempData = [
+  {
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book a Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: false,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: false,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+];
+
+export default tempData;
